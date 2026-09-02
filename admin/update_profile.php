e8acfad6a81cf9fd8858ead6993b98ee650e6716
Sachila Jayanga <171365598+SachilaJayanga@users.noreply.github.com>
2026-09-02 14:24:15 +0530

    Update update_profile.php

--- admin/update_profile.php
+++ admin/update_profile.php
@@ -9,19 +9,15 @@ if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $current_image = $_POST['current_image'];
 
-    // Check if a new image is uploaded
-    if (isset($_FILES['new_image']['name']) && $_FILES['new_image']['name'] != "") {
-        // Handle file upload (e.g., moving to the correct directory)
-        $image_name = $_FILES['new_image']['name'];
+     if (isset($_FILES['new_image']['name']) && $_FILES['new_image']['name'] != "") {
+         $image_name = $_FILES['new_image']['name'];
         $image_tmp = $_FILES['new_image']['tmp_name'];
         move_uploaded_file($image_tmp, "../images/icon/" . $image_name);
     } else {
-        // No new image uploaded, retain current image
-        $image_name = $current_image;
+         $image_name = $current_image;
     }
 
-    // Update the database with the new information
-    $sql = "UPDATE tbl_accounts SET
+     $sql = "UPDATE tbl_accounts SET
         full_name = '$full_name',
         email = '$email',
         username = '$username',
