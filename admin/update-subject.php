19b59853b076dc72cea5c2613cdf6e3ad64d0f43
Sachila Jayanga <171365598+SachilaJayanga@users.noreply.github.com>
2026-09-02 14:22:58 +0530

    Update update-subject.php

--- admin/update-subject.php
+++ admin/update-subject.php
@@ -39,41 +39,30 @@ include('partials/navbar.php');
 
         <?php 
         
-            //Check whether the id is set or not
-            if(isset($_GET['id']))
-            {
-                //Get the ID and all other details
-                //echo "Getting the Data";
+             if(isset($_GET['id']))
+            { 
                 $id = $_GET['id'];
-                //Create SQL Query to get all other details
-                $sql = "SELECT * FROM tbl_subjects WHERE id=$id";
-
-                //Execute the Query
-                $res = mysqli_query($conn, $sql);
-
-                //Count the Rows to check whether the id is valid or not
+                 $sql = "SELECT * FROM tbl_subjects WHERE id=$id"; 
+                $res = mysqli_query($conn, $sql); 
                 $count = mysqli_num_rows($res);
 
                 if($count==1)
                 {
-                    //Get all the data
-                    $row = mysqli_fetch_assoc($res);
+                     $row = mysqli_fetch_assoc($res);
                     $title = $row['title'];
                     $current_image = $row['image_name'];
                     $active = $row['active'];
                 }
                 else
                 {
-                    //redirect to manage category with session message
-                    $_SESSION['no-subject-found'] = "<div class='error'>Category not Found.</div>";
+                     $_SESSION['no-subject-found'] = "<div class='error'>Category not Found.</div>";
                     header('location:manage-subject.php');
                 }
 
             }
             else
             {
-                //redirect to Manage CAtegory
-                header('location:manage-subject.php');
+                 header('location:manage-subject.php');
             }
         
         ?>
@@ -94,15 +83,13 @@ include('partials/navbar.php');
                         <?php 
                             if($current_image != "")
                             {
-                                //Display the Image
-                                ?>
+                                 ?>
                                 <img src="../images/category/<?php echo $current_image; ?>" width="150px">
                                 <?php
                             }
                             else
                             {
-                                //Display Message
-                                echo "<div class='error'>Image Not Added.</div>";
+                                 echo "<div class='error'>Image Not Added.</div>";
                             }
                         ?>
                     </td>
@@ -113,10 +100,7 @@ include('partials/navbar.php');
                     <td>
                         <input type="file" name="image">
                     </td>
-                </tr>
-
-                
-
+                </tr> 
                 <tr>
                     <td>Active: </td>
                     <td>
@@ -141,48 +125,32 @@ include('partials/navbar.php');
         <?php 
         
             if(isset($_POST['submit']))
-            {
-                //echo "Clicked";
-                //1. Get all the values from our form
+            { 
                 $id = $_POST['id'];
                 $title = $_POST['title'];
                 $current_image = $_POST['current_image'];
                 $active = $_POST['active'];
-
-                //2. Updating New Image if selected
-                //Check whether the image is selected or not
+ 
                 if (isset($_FILES['image']['name'])) {
-                    // Get the Image Details
-                    $image_name = $_FILES['image']['name'];
+                     $image_name = $_FILES['image']['name'];
                 
-                    // Check whether the image is available or not
-                    if ($image_name != "") {
-                        // Image Available
-                
-                        // A. Upload the New Image
-                        $extArray = explode('.', $image_name); // Fixed here
-                        $ext = end($extArray); // Fixed here
-                
-                        // Rename the Image
-                        $image_name = "Note_Category_" . rand(000, 999) . '.' . $ext; // e.g. Note_Category_834.jpg
-                
+                     if ($image_name != "") { 
+                        $extArray = explode('.', $image_name);  
+                        $ext = end($extArray);   
+                        $image_name = "Note_Category_" . rand(000, 999) . '.' . $ext;  
+ 
                         $source_path = $_FILES['image']['tmp_name'];
-                        $destination_path = "../images/category/" . $image_name;
-                
-                        // Finally Upload the Image
+                        $destination_path = "../images/category/" . $image_name; 
                         $upload = move_uploaded_file($source_path, $destination_path);
-                
-                        // Check whether the image is uploaded or not
+                 
                         if ($upload == false) {
                             $_SESSION['upload'] = "<div class='error'>Failed to Upload Image. </div>";
                             header('location:manage-subject.php');
                             exit();
-                        }
-                
-                        // B. Remove the Current Image if available
+                        } 
                         if ($current_image != "") {
                             $remove_path = "../images/category/" . $current_image;
-                            if (file_exists($remove_path)) { // Check if file exists
+                            if (file_exists($remove_path)) {  
                                 $remove = unlink($remove_path);
                             } else {
                                 $_SESSION['failed-remove'] = "<div class='error'>Current Image file does not exist.</div>";
@@ -193,33 +161,23 @@ include('partials/navbar.php');
                     }
                 } else {
                     $image_name = $current_image;
-                }
-                
-
-                //3. Update the Database
+                } 
                 $sql2 = "UPDATE tbl_subjects SET 
                     title = '$title',
                     image_name = '$image_name',
                     active = '$active' 
-                    WHERE id=$id
-                ";
+                    WHERE id=$id";
 
-                //Execute the Query
-                $res2 = mysqli_query($conn, $sql2);
-
-                //4. REdirect to Manage Category with MEssage
-                //CHeck whether executed or not
+                 $res2 = mysqli_query($conn, $sql2); 
                 if($res2==true)
                 {
-                    //Category Updated
-                    $_SESSION['update'] = "<div class='success'>Subject Updated Successfully.</div>";
+                     $_SESSION['update'] = "<div class='success'>Subject Updated Successfully.</div>";
                     header('location:manage-subject.php');
                     
                 }
                 else
                 {
-                    //failed to update category
-                    $_SESSION['update'] = "<div class='error'>Failed to Update Subject.</div>";
+                     $_SESSION['update'] = "<div class='error'>Failed to Update Subject.</div>";
                     header('location:manage-subject.php');
                 }
 
